22c4f3ba11f770d385f1ac5d01354dde67a840ed
kenchartren <gjj3kenchartren@163.com>
2018-03-21 09:12:32 +0800

    Revert "new 1"
    This reverts commit 0bfd18c2d479624d03ddfeee6a32b01e0de9fb9f.

--- fib_r.R
+++
@@ -1,4 +0,0 @@
-fib_R <- function(n){
-  if(n==1||n==2) return(1)
-  return(fib_R(n-1)+fib_R(n-2))
-}
--- mfibR.R
+++
@@ -1,8 +0,0 @@
-setwd("C:\\Users\\jiaju\\Documents\\Github\\mini_program")
-
-source('fib_r.R')
-
-Rcpp::sourceCpp('fibcpp.cpp')
-
-system.time(fib_R(35))
-system.time(fib_cpp_1(35))
